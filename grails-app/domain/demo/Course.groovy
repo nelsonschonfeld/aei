63dd66513f630eee39d44ab5839b36452e1ddd19
nelsonschonfeld <nelson_schonfeld@hotmail.com>
2019-04-02 12:18:16 -0300

    people domain course and inscription

---
+++ grails-app/domain/demo/Course.groovy
@@ -0,0 +1,25 @@
+package demo
+
+class Course {
+    String name
+    String type
+    String year
+    String teacher
+    Date schedule
+    Long amount
+    Long dueCost
+    Long printCost
+    Boolean monday
+    Boolean thursday
+    Boolean wednesday
+    Boolean tuesday
+    Boolean friday
+    Boolean saturday
+    Date dateCreated
+    Date lastUpdated
+    Boolean active
+    String status
+
+    static constraints = {
+    }
+}
---
+++ grails-app/domain/demo/Inscription.groovy
@@ -0,0 +1,14 @@
+package demo
+
+class Inscription {
+    People student
+    Course course
+    Long discountAmount
+    String status
+    Boolean active
+    Date dateCreated
+    Date lastUpdated
+
+    static constraints = {
+    }
+}
