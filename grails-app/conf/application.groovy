42228abd4e28e85ab089670744826a0afa2dc1b8
nelsonschonfeld <nelson_schonfeld@hotmail.com>
2019-11-22 19:38:45 -0300

    fix logout and user db

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -1,5 +1,3 @@
-
-
 // Added by the Spring Security Core plugin:
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'aei.User'
 grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'aei.UserRole'
@@ -25,4 +23,6 @@ grails.plugin.springsecurity.filterChain.chainMap = [
 	[pattern: '/**/favicon.ico', filters: 'none'],
 	[pattern: '/**',             filters: 'JOINED_FILTERS']
 ]
+
+grails.plugin.springsecurity.logout.postOnly = false
 
--- grails-app/init/aei/BootStrap.groovy
+++ grails-app/init/aei/BootStrap.groovy
@@ -4,26 +4,29 @@ import enums.CourseStatusEnum
 import enums.FeeStatusEnum
 import enums.Months
 import enums.PersonTypeEnum
+import grails.util.Environment
 
 class BootStrap {
 
     def init = { servletContext ->
 
-        final boolean flush = true
-        final boolean failOnError = true
-        def role = new Role(authority: 'ROLE_ADMIN')
-        role.save(flush: flush, failOnError: true)
-        def user = new User(username: 'juanpablo', password: 'admjpablo', fullName: 'Administrador')
-        user.save(flush: flush)
-        def userRole = new UserRole(role: role, user: user)
-        userRole.save(flush: flush, failOnError: failOnError)
+        if (Environment.current == Environment.DEVELOPMENT) {
+            final boolean flush = true
+            final boolean failOnError = true
+            def role = new Role(authority: 'ROLE_ADMIN')
+            role.save(flush: flush, failOnError: true)
+            def user = new User(username: 'juanpablo', password: 'admjpablo', fullName: 'Administrador')
+            user.save(flush: flush)
+            def userRole = new UserRole(role: role, user: user)
+            userRole.save(flush: flush, failOnError: failOnError)
 
-        def roleSec = new Role(authority: 'ROLE_SECRETARIA')
-        roleSec.save(flush: flush, failOnError: true)
-        def userSec = new User(username: 'secretaria', password: 'aeisecret', fullName: 'Secretaria')
-        userSec.save(flush: flush)
-        def userRoleSec = new UserRole(role: roleSec, user: userSec)
-        userRoleSec.save(flush: flush, failOnError: failOnError)
+            def roleSec = new Role(authority: 'ROLE_SECRETARIA')
+            roleSec.save(flush: flush, failOnError: true)
+            def userSec = new User(username: 'secretaria', password: 'aeisecret', fullName: 'Secretaria')
+            userSec.save(flush: flush)
+            def userRoleSec = new UserRole(role: roleSec, user: userSec)
+            userRoleSec.save(flush: flush, failOnError: failOnError)
+        }
     }
 
     def destroy = {
