7f8f15cf6d103514527b39b09b2c0d1b4373691b
Ayelen <cianayelen@gmail.com>
2020-04-18 15:19:16 -0300

    Cash (#33)
    Update index

--- grails-app/domain/aei/Cash.groovy
+++ grails-app/domain/aei/Cash.groovy
@@ -1,13 +1,14 @@
 package aei
 
 class Cash {
-
+  
+    Date dateCreated = new Date()
     Double initalAmount
     Double costs
     String comment
-    Date dateCreated
     String user
 
     static constraints = {
+        dateCreated(nullable:false, unique:true)
     }
 }
